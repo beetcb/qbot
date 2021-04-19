3676ffe96a063fd2987be92c2d3983ca2a985c81
beetcb <63141491+beetcb@users.noreply.github.com>
2021-04-19 21:14:44 +0800

    revert: disable http report, somewhat buggy

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,12 @@ import { transPinyin } from './plugin/transPinyin'
 require('koishi-adapter-onebot')
 
 const config = {
-  type: 'onebot:http',
-  port: '5701',
-  server: 'http://localhost:5700',
+  type: 'onebot:ws',
+  server: 'ws://localhost:6700',
 }
 
 const app = new App(config as any)
 
-// 注册插件，作用相当于上面配置文件中的 plugins 部分
 app.plugin(transPinyin).plugin(talkBot)
 
-// 启动应用
 app.start()
